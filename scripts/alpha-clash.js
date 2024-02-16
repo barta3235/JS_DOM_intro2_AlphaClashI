e7ab6b8a842a6a8e4b77bca059a1b05f62e9f5d0
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-02-16 17:21:16 +0600

    navigating from home section to play section

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,17 @@
+// function play(){
+//     //hide the home screen by adding hidden class in home screen
+//     const homeScreen= document.getElementById('home');
+//     //homeScreen.classList prints all the classes in the section
+//     homeScreen.classList.add('hidden');
+    
+//     const playSection= document.getElementById('play');
+//     playSection.classList.remove('hidden');
+
+//     //show the play ground
+// }
+
+
 function play(){
-    console.log('Connects')
+    hideElementsById('home');
+    showElementById('play');
 }
